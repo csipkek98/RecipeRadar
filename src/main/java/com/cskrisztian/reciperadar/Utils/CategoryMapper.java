233c4a66c53aab9b89bc110bdcaadd933b48beda
csipkek98 <csipkekrisztian@gmail.com>
2025-07-13 18:42:25 +0200

    Add: toDto method

--- src/main/java/com/cskrisztian/reciperadar/Utils/CategoryMapper.java
+++ src/main/java/com/cskrisztian/reciperadar/Utils/CategoryMapper.java
@@ -1,18 +1,30 @@
 package com.cskrisztian.reciperadar.Utils;
 
 import com.cskrisztian.reciperadar.DTO.CategoriesDTO;
-import com.cskrisztian.reciperadar.Entities.Categories;
+import com.cskrisztian.reciperadar.Entities.Categorie;
 
 public class CategoryMapper {
-    public static Categories toEntity(CategoriesDTO categoriesDTO) {
+    public static Categorie toEntity(CategoriesDTO categoriesDTO) {
         if (categoriesDTO == null) {
             return null;
         }
-        Categories category = new Categories();
+        Categorie category = new Categorie();
         category.setId(Long.parseLong(categoriesDTO.getIdCategory()));
         category.setCategory_name(categoriesDTO.getStrCategory());
         category.setCategory_thumbnail_url(categoriesDTO.getStrCategoryThumb());
         category.setCategory_description(categoriesDTO.getStrCategoryDescription());
         return category;
+    }
+
+    public static CategoriesDTO toDTO(Categorie categorie) {
+        if (categorie == null) {
+            return null;
+        }
+        CategoriesDTO categoriesDTO = new CategoriesDTO();
+        categoriesDTO.setIdCategory(String.valueOf(categorie.getId()));
+        categoriesDTO.setStrCategory(categorie.getCategory_name());
+        categoriesDTO.setStrCategoryThumb(categorie.getCategory_thumbnail_url());
+        categoriesDTO.setStrCategoryDescription(categorie.getCategory_description());
+        return categoriesDTO;
     }
 }
